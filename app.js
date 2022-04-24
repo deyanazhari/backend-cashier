2f51cfb4a5b613245fed6ab3d8e03398e5967513
deyanazhari <deyanazhari@gmail.com>
2022-04-25 04:04:43 +0700

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
+app.post('/categories', indexRouter);
 // mongoose.connect('mongodb://localhost:27017/');
 
 //connect to mongodb
