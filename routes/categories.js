c515eff6a3d005372f81348733fb643095797755
deyanazhari <deyanazhari@gmail.com>
2022-05-09 22:34:49 +0700

    update

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {index, store} from './controllers/CategoryController.js';
-router.get('/categories',index);
-router.post('/categories',store);
+router.get('/',index);
+router.post('/',store);
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 import express from 'express';
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
-});
+
+router.use('/categories',categories);
 
 export default router;
