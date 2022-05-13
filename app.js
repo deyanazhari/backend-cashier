4a779fc721303961e8a367d7011b366a0b2c00c4
deyanazhari <deyanazhari@gmail.com>
2022-05-14 04:17:31 +0700

    fix it cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import indexRouter  from './routes/index.js';
 const env = dotenv.config().parsed;
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/', indexRouter);
 
 // mongoose.connect('mongodb://localhost:27017/');
